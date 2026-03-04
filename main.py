e2b55b6734bb16676e486c38aaca50468ce2a226
andrii-h-322 <andreiman1620@gmail.com>
2026-03-05 00:09:48 +0100

    fix handlers import

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from handlers import user
+import handlers as user_module
+
+
 
 async def main():
     bot = Bot(token='8738202781:AAGyUhiO4vJndwj8X2p_03vdxhnRfxYiGWI')
     dp = Dispatcher()
-    dp.include_router(user)  # ← просто user, без .user
+    dp.include_router(user_module.user)  # ← просто user, без .user
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
