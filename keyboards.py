a1fffe5d2b58262b2c377dab672817086a8baf02
andrii-h-322 <andreiman1620@gmail.com>
2026-03-05 11:52:39 +0100

    add days together feature

--- keyboards.py
+++ keyboards.py
@@ -3,8 +3,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardBut
 menu = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Рассказать о лучшей девушке')],
-        [KeyboardButton(text='Показать коплименты'),KeyboardButton(text='Нажми сюда')],
-        [KeyboardButton(text='Счетчик')]
+        [KeyboardButton(text='Показать коплименты'), KeyboardButton(text='Нажми сюда')],
+        [KeyboardButton(text='Счетчик'), KeyboardButton(text='Напиши мне письмо')]
     ],
     resize_keyboard=True,
     input_field_placeholder='Выберите действие'
@@ -19,3 +19,11 @@ catalog = InlineKeyboardMarkup(
         [InlineKeyboardButton(text='Profil sozdatelya', url='https://t.me/xarok228')]
     ]
 )
+letter = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Почему я тебя люблю', callback_data='why_love')],
+        [InlineKeyboardButton(text='Мои чувства к тебе', callback_data='feelings')],
+        [InlineKeyboardButton(text='Почему я полюбил тебя и ты мне понравилась', callback_data='why_love_first')]
+    ]
+)
+
